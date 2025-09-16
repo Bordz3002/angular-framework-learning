3234a87a6504731f955380580c1c88ee24af48ad
naval <109155702+Bordz3002@users.noreply.github.com>
2025-09-16 21:33:13 +0800

    content child: /QueryList/ContentChildren/subscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import {Component, ViewEncapsulation, ChangeDetectionStrategy} from '@angular/core';
-import {ContentChildDemo4Component} from './components/communication/content-child/demo4/demo4.component';
+import{
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy
+} from '@angular/core';
+import {ContentChildDemo4} from './components/communication/content-child/demo4/demo4.component';
 @Component({
   selector:'app-root',
   standalone:true,
-  imports:[ContentChildDemo4Component],
+  imports:[ContentChildDemo4],
   templateUrl:'./app.component.html',
   styleUrl:'./app.component.css',
   encapsulation:ViewEncapsulation.Emulated,
--- src/app/components/communication/content-child/demo4/demo4.component.ts
+++ src/app/components/communication/content-child/demo4/demo4.component.ts
@@ -1,4 +1,12 @@
-import {Component, ViewEncapsulation, ChangeDetectionStrategy, ContentChild, ElementRef, AfterContentInit} from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+  ContentChildren,
+  ElementRef,
+  AfterContentInit,
+  QueryList
+} from '@angular/core';
 @Component({
   selector:'content-child-demo4',
   standalone:true,
@@ -8,10 +16,16 @@ import {Component, ViewEncapsulation, ChangeDetectionStrategy, ContentChild, Ele
   encapsulation:ViewEncapsulation.Emulated,
   changeDetection:ChangeDetectionStrategy.Default
 })
-export class ContentChildDemo4Component{
-  @ContentChild('projectedContent') projectedElement!:ElementRef;
+export class ContentChildDemo4{
+  @ContentChildren('contentToProject') content!:QueryList<ElementRef>;
   constructor(){};
   ngAfterContentInit():void{
-    this.projectedElement.nativeElement.style.color='blue';
+    this.content.forEach((element, iteration):void=>{
+      element.nativeElement.style.color=(iteration % 2 ===0)? 'blue':'orange';
+      element.nativeElement.style.fontWeight='bolder';
+    });
+    this.content.changes.subscribe((list):void=>{
+      console.log(`contents changed: ${list.length}`);
+    });
   }
 }
